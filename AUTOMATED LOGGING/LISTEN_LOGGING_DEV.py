f11fba32e8a6a19a6f58c08d868a27f497b9e196
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-04-02 15:40:32 -0400

    Updated snapshot logging. Added order_line table to snapshot logging. Dropped user_name and station columns from snapshot tables. Added comments to new functions. Added column name/row value string functions. Added verbose print statements. Added snapshot functions. Added snapshot timer to main.

--- AUTOMATED LOGGING/LISTEN_LOGGING_DEV.py
+++ AUTOMATED LOGGING/LISTEN_LOGGING_DEV.py
@@ -1,6 +1,7 @@
 import json
 import re
 import datetime
+from threading import Timer
 from sigm import *
 
 # PostgreSQL DB connection configs
@@ -26,8 +27,8 @@ INC_TABLES = [
 # Snapshot log tables
 SNAP_TABLES = [
     'bill_of_materials_mat',
+    'order_line',
     'part_kit',
-    'contract',
     'contract_group_line',
     'contract_part_line'
 ]
@@ -41,9 +42,7 @@ INC_COLUMNS = [
 ]
 
 SNAP_COLUMNS = [
-    ['time_stamp', 'TIMESTAMP WITHOUT TIME ZONE'],
-    ['user_name', 'TEXT'],
-    ['station', 'TEXT']
+    ['time_stamp', 'TIMESTAMP WITHOUT TIME ZONE']
 ]
 
 
@@ -63,11 +62,53 @@ def table_columns(table, cursor):
     return columns
 
 
+# Select an entire table
 def whole_table(table_name, cursor):
     sql_exp = f'SELECT * FROM {table_name}'
     result_set = sql_query(sql_exp, cursor)
     table = tabular_data(result_set)
     return table
+
+
+# Pass table name, return comma separated string of column names, optionally include attributes
+def column_name_str(table_name, cursor, include_attributes=False):
+    raw_columns = table_columns(table_name, cursor)
+    columns = []
+    str_columns = ''
+    for column in raw_columns:
+        if include_attributes:
+            pair = []
+            for cell in column:
+                pair.append(cell)
+            str_pair = ' '.join(pair)
+            columns.append(str_pair)
+        else:
+            column_name = column[0]
+            columns.append(column_name)
+        str_columns = ', '.join(columns)
+    return str_columns
+
+
+# Pass row, return comma separated string of values
+def row_value_str(row):
+    values = []
+    for value in row:
+        if value is None:
+            value = 'Null'
+        if type(value) in (datetime.date, datetime.time):
+            value = str(value)
+        if type(value) != str:
+            value = str(value)
+        else:
+            if value != 'Null':
+                if "'" in value:
+                    value = value.replace("'", "''")
+                if "\\" in value:
+                    value = value.replace("\\", "\\\\")
+                value = "'" + value + "'"
+        values.append(value)
+    str_values = ', '.join(values)
+    return str_values
 
 
 # Add incremental log triggers to all tables in INC_TABLES list
@@ -95,37 +136,103 @@ def drop_triggers():
 
 # Create incremental log tables on LOG DB for every table in INC_TABLES list
 def add_tables(table_list):
-    for table in table_list:
-        raw_columns = table_columns(table, sigm_db_cursor)
-        columns = []
-        for column in raw_columns:
-            pair = []
-            for cell in column:
-                pair.append(cell)
-            str_pair = ' '.join(pair)
-            columns.append(str_pair)
-        str_columns = ', '.join(columns)
+    for table_name in table_list:
+        str_columns = column_name_str(table_name, sigm_db_cursor, True)
 
         if table_list == SNAP_TABLES:
-            table += '_snap'
+            table_name += '_snap'
 
-        sql_exp = f'CREATE TABLE IF NOT EXISTS {table}(' \
+        sql_exp = f'CREATE TABLE IF NOT EXISTS {table_name}(' \
                   f'{str_columns})'
         log_db_cursor.execute(sql_exp)
-        print(f'{table} log table checked.')
+        print(f'{table_name} log table checked.')
 
 
 # Initialize snapshot logging tables.
-def init_snap_tables(table_list):
+def init_snap_tables():
     timestamp = datetime.datetime.now()
-    for table in table_list:
-        dest_table = table + '_snap'
-        if not whole_table(dest_table, log_db_cursor):
-            copy_table(table, dest_table, sigm_db_cursor, log_db_cursor)
-            sql_exp = f'UPDATE {dest_table} ' \
-                      f'SET time_stamp = \'{timestamp}\', ' \
-                      f'user_name = \'INIT\', ' \
-                      f'station = \'fileserver\''
+    for table_name in SNAP_TABLES:
+        dest_table_name = table_name + '_snap'
+        if not whole_table(dest_table_name, log_db_cursor):
+            copy_table(table_name, dest_table_name, sigm_db_cursor, log_db_cursor)
+            sql_exp = f'UPDATE {dest_table_name} ' \
+                      f'SET time_stamp = \'{timestamp}\''
+            log_db_cursor.execute(sql_exp)
+
+
+# Set snapshot timer delay, optionally delay by 24 hours (for every snapshot subsequent to the first)
+def set_snap_timer(delay=False):
+    now = datetime.datetime.today()
+    then = now.replace(day=now.day, hour=23, minute=0, second=0, microsecond=0)
+
+    delta = then - now
+    secs = delta.seconds if delay is False else delta.seconds + 86400
+    hours = round((secs / 60) / 60, 2)
+    print(f'Snapshot timer scheduled for {hours} hours from now.')
+    return secs
+
+
+# Start snapshot timer, optionally delay by 24 hours (for every snapshot subsequent to the first)
+def start_snap_timer(delay=False):
+    secs = set_snap_timer(delay)
+    snap_timer = Timer(secs, snapshot_handler)
+    snap_timer.start()
+
+
+# Checks inc tables for records added today, writes a snapshot of the record, starts timer for next snapshot
+def snapshot_handler():
+    timestamp = str(datetime.datetime.now())
+    for table_name in SNAP_TABLES:
+        ref_name = check_inc_table(table_name, timestamp)
+        if ref_name:
+            write_snap_log(table_name, ref_name, timestamp)
+    start_snap_timer(True)
+
+
+# Checks inc table for records added today, gets ref_name if new increments exist
+def check_inc_table(table_name, timestamp):
+    sql_exp = f'SELECT * FROM {table_name} WHERE time_stamp::DATE = \'{timestamp}\'::DATE'
+    result_set = sql_query(sql_exp, log_db_cursor)
+    table = tabular_data(result_set)
+    if table:
+        ref_name = snap_log_ref(table_name)
+        return ref_name
+
+
+# Return appropriate column to use as reference when writing to snap log
+def snap_log_ref(table_name):
+    ref_name = ''
+    if table_name == 'bill_of_materials_mat':
+        ref_name = 'prt_master_id'
+    elif table_name == 'order_line':
+        ref_name = 'ord_id'
+    elif table_name == 'part_kit':
+        ref_name = 'pkt_master_prt_id'
+    elif table_name == 'contract_group_line':
+        ref_name = 'con_id'
+    elif table_name == 'contract_part_line':
+        ref_name = 'con_id'
+    return ref_name
+
+
+def write_snap_log(table_name, ref_name, timestamp):
+    str_columns = column_name_str(table_name, sigm_db_cursor)
+
+    sql_exp = f'SELECT DISTINCT {ref_name} FROM {table_name} WHERE time_stamp::DATE = \'{timestamp}\'::DATE'
+    result_set = sql_query(sql_exp, log_db_cursor)
+    ref_table = tabular_data(result_set)
+    for ref_row in ref_table:
+        ref = ref_row[0]
+        sql_exp = f'SELECT * FROM {table_name} WHERE {ref_name} = {ref}'
+        result_set = sql_query(sql_exp, sigm_db_cursor)
+        table_data = tabular_data(result_set)
+        rows = len(table_data)
+        print(f'Writing {rows} rows to {table_name}_snap for {ref_name} {ref}')
+        for row in table_data:
+            str_values = row_value_str(row)
+            sql_exp = \
+                f'INSERT INTO {table_name}_snap ({str_columns}, time_stamp) ' \
+                f'VALUES ({str_values}, \'{timestamp}\')'
             log_db_cursor.execute(sql_exp)
 
 
@@ -162,13 +269,7 @@ def copy_table(source_table_name, dest_table_name, source_cursor, dest_cursor=No
     if dest_cursor is None:
         dest_cursor = source_cursor
 
-    columns = table_columns(source_table_name, sigm_db_cursor)
-    column_names = []
-    for column in columns:
-        column_name = column[0]
-        column_names.append(column_name)
-    str_columns = ', '.join(column_names)
-
+    str_columns = column_name_str(source_table_name, sigm_db_cursor)
     source_table = whole_table(source_table_name, sigm_db_cursor)
     rows = len(source_table)
 
@@ -176,25 +277,9 @@ def copy_table(source_table_name, dest_table_name, source_cursor, dest_cursor=No
         f'Copying {rows} rows table {source_table_name} using {source_cursor} to {dest_table_name} using {dest_cursor}')
 
     for row in source_table:
-        values = []
-        for value in row:
-            if value is None:
-                value = 'Null'
-            if type(value) == datetime.date:
-                value = str(value)
-            if type(value) != str:
-                value = str(value)
-            else:
-                if value != 'Null':
-                    if "'" in value:
-                        value = value.replace("'", "''")
-                    value = "'" + value + "'"
-            values.append(value)
-        str_values = ', '.join(values)
-
+        str_values = row_value_str(row)
         sql_exp = fr'INSERT INTO {dest_table_name} ({str_columns}) VALUES ({str_values})'
         dest_cursor.execute(sql_exp)
-
     print(f'Copying table {source_table_name} to {dest_table_name} complete.')
 
 
@@ -231,27 +316,14 @@ def alert_handler(alert_dict):
         columns.append(key)
     str_columns = ', '.join(columns)
 
-    values = []
-    for value in alert_dict.values():
-        if value is None:
-            value = 'Null'
-        if type(value) == datetime.date:
-            value = f'\'{value}\'::DATE'
-        if type(value) != str:
-            value = str(value)
-        else:
-            if value != 'Null':
-                if "'" in value:
-                    value = value.replace("'", "''")
-                value = "'" + value + "'"
-        values.append(value)
-    str_values = ', '.join(values)
+    row = alert_dict.values()
+    str_values = row_value_str(row)
 
     return str_columns, str_values
 
 
 # Insert named variables returned by payload handler and payload generated by alert_handler into log table
-def log_handler(alert_table, str_columns, timestamp, user, station, alert_age, alert_tg_op, str_values):
+def write_inc_log(alert_table, str_columns, timestamp, user, station, alert_age, alert_tg_op, str_values):
     sql_exp = fr"INSERT INTO {alert_table} (tg_op, time_stamp, user_name, station, age, {str_columns}) " \
               fr"VALUES ('{alert_tg_op}', '{timestamp}', '{user}', '{station}', '{alert_age}', {str_values})"
     log_db_cursor.execute(sql_exp)
@@ -275,7 +347,8 @@ def main():
     extend_tables(INC_TABLES, INC_COLUMNS, log_db_cursor)
     extend_tables(SNAP_TABLES, SNAP_COLUMNS, log_db_cursor)
 
-    init_snap_tables(SNAP_TABLES)
+    init_snap_tables()
+    start_snap_timer(False)
 
     while 1:
         try:
@@ -297,7 +370,7 @@ def main():
 
                 alert_table, alert_dict, timestamp, user, station, alert_age, alert_tg_op = payload_handler(raw_payload)
                 str_columns, str_values = alert_handler(alert_dict)
-                log_handler(alert_table, str_columns, timestamp, user, station, alert_age, alert_tg_op, str_values)
+                write_inc_log(alert_table, str_columns, timestamp, user, station, alert_age, alert_tg_op, str_values)
 
 
 main()
